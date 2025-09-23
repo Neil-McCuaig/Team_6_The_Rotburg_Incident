b9c838f1f1643cb5e6c056989c7bbf7e4c8da9b3
Park <cjparkerdj@gmail.com>
2025-09-22 23:50:50 -0400

    Made Stun ability modular to any enemy script

--- The-RotBurg-Incident/Assets/Scripts/EnemyController.cs
+++ The-RotBurg-Incident/Assets/Scripts/EnemyController.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class EnemyController : MonoBehaviour, EnemyStunable
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 velocity;
     private SpriteRenderer spriteRenderer;
+    private Animator enemyAnim;
 
     bool facingRight = true;
     bool hitWall = false;
-    public bool isStunned = false;
+    public bool isStunned;
+    public float stunTimer;
+    private float stunCountdown;
 
     private Transform wallCheck;
     public LayerMask groundLayer;
@@ -20,14 +23,20 @@ public class EnemyController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        enemyAnim = GetComponent<Animator>();
         wallCheck = transform.Find("WallCheck");
+
+        isStunned = false;
+        stunCountdown = stunTimer;
     }
 
     private void Update()
     {
         hitWall = Physics2D.OverlapCircle(wallCheck.position, 0.1f, groundLayer);
+
         if (!isStunned)
         {
+            enemyAnim.SetBool("isStunned", false);
             if (facingRight)
             {
                 velocity.x = moveSpeed;
@@ -56,9 +65,25 @@ public class EnemyController : MonoBehaviour
         }
         else
         {
-            velocity.x = 0f;
+            enemyAnim.SetBool("isStunned", true);
+
+            stunCountdown -= Time.deltaTime;
+            if (stunCountdown <= 0f)
+            {
+                isStunned = false;
+                stunCountdown = stunTimer;
+                rb.WakeUp();
+            }
+            return;
         }
 
         rb.velocity = velocity;
+    }
+
+    public void Stun()
+    {
+        isStunned = true;
+        rb.Sleep();
+        Debug.Log("Stunned");
     }
 }
--- The-RotBurg-Incident/Assets/Scripts/StunHitBox.cs
+++ The-RotBurg-Incident/Assets/Scripts/StunHitBox.cs
@@ -4,27 +4,16 @@ using UnityEngine;
 
 public class StunHitBox : MonoBehaviour
 {
-    private Animator enemyAnim;
-    private EnemyController enemyController;
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy"))
         {
-            enemyAnim = other.GetComponent<Animator>();
-            enemyController = other.GetComponent<EnemyController>();
-
-            Debug.Log("Player hit Enemy");
-            StartCoroutine(StunTimer());
-            enemyController.isStunned = true;
-            enemyAnim.SetBool("isStunned", true);
+            Debug.Log("Enemy Stun");
+            EnemyStunable stunnable = other.GetComponent<EnemyStunable>();
+            if (stunnable != null)
+            {
+                stunnable.Stun();
+            }
         }
-    }
-
-    IEnumerator StunTimer()
-    {
-        yield return new WaitForSeconds(0.9f);
-        enemyAnim.SetBool("isStunned", false);
-        enemyController.isStunned = false;
     }
 }
