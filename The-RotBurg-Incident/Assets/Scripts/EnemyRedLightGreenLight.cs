f0faa95ae839fff2e1cee9cfcbc3aedf885d0ce4
Neil-McCuaig <116813845+Neil-McCuaig@users.noreply.github.com>
2025-10-08 18:05:15 -0400

    Added Phases for Blackguard
    Added Phases for the Blackguard enemy. Not all functions included.

---
+++ The-RotBurg-Incident/Assets/Scripts/EnemyRedLightGreenLight.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRedLightGreenLight : MonoBehaviour
+{
+    public float maxPhaseTimer = 0f;
+    public float currentPhaseTimer = 0f;
+    public DomainZoneLogic DomainZone;
+    private PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = FindAnyObjectByType<PlayerController>();
+        DomainZone = FindAnyObjectByType<DomainZoneLogic>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SleepPhase()
+    {
+
+        //Maybe change some of these to a "While" or otherwise add some sort of a cutoff for if the player just leaves in the middle of
+        //a phase?
+        if (DomainZone.playerInDomain == true)
+        {
+            //MaxPhaseTimer should be randomized here, and every time it gets called for a new phase, at random, in a small range.
+            //Leave the player guessing a bit. Just doing it as 5 so I can process with other code for the moment.
+            maxPhaseTimer = 5f;
+            currentPhaseTimer = maxPhaseTimer;
+            currentPhaseTimer -= Time.deltaTime;
+
+            if (currentPhaseTimer < 0f && playerController.inLocker == false)
+            {
+                PhaseGreen();
+            }
+        }
+    }
+
+    private void PhaseGreen() 
+    {
+        maxPhaseTimer = 5f;
+        currentPhaseTimer = maxPhaseTimer;
+        currentPhaseTimer -= Time.deltaTime;
+
+        if (currentPhaseTimer < 0f && playerController.inLocker == false)
+        {
+            PhaseYellow();
+        }
+    }
+
+    private void PhaseYellow()
+    {
+        maxPhaseTimer = 5f;
+        currentPhaseTimer = maxPhaseTimer;
+        currentPhaseTimer -= Time.deltaTime;
+
+        if (currentPhaseTimer < 0f && playerController.inLocker == false)
+        {
+            PhaseRed();
+        }
+        //Originally put this here. Maybe it would work better is phase red?
+        //else if (currentPhaseTimer < 0f && playerController.inLocker == true)
+        //{
+            //SleepPhase();
+        //}
+    }
+
+    private void PhaseRed()
+    {
+        maxPhaseTimer = 5f;
+        currentPhaseTimer = maxPhaseTimer;
+        currentPhaseTimer -= Time.deltaTime;
+        if (currentPhaseTimer < 0f && playerController.inLocker == true)
+        {
+            SleepPhase();
+        }
+        else if (currentPhaseTimer < 0f && playerController.inLocker == false)
+        {
+            AttackPhase();
+        }
+    }
+
+    private void AttackPhase()
+    {
+
+    }
+}
