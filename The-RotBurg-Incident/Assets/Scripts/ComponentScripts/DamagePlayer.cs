8aeb02207ed4d487a96a9d3615bcb2347f55bf4f
Park <cjparkerdj@gmail.com>
2026-03-24 22:19:33 -0400

    Added a new UI attack effect for the hall monitor

--- The-RotBurg-Incident/Assets/Scripts/ComponentScripts/DamagePlayer.cs
+++ The-RotBurg-Incident/Assets/Scripts/ComponentScripts/DamagePlayer.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class DamagePlayer : MonoBehaviour
 {
     public float damageAmount;
+    private UIEyeballAttackEffect attackEffect;
+
+    private void Start()
+    {
+        attackEffect = FindAnyObjectByType<UIEyeballAttackEffect>();
+    }
 
     public void ApplyDamageToPlayer()
     {
         FindAnyObjectByType<PlayerHealth>().TakeDamage(damageAmount);
+        if (attackEffect != null)
+        {
+            attackEffect.ActivateOverlay();
+
+            Debug.Log("Triggered");
+        }
     }
 }
